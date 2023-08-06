2efbaa030d59d26b259abfd61bab8521ff89717f
John Princen <jprincen@gmail.com>
2023-08-06 14:12:09 -0700

    Add exception handling

--- stack/align_images.py
+++ stack/align_images.py
@@ -9,8 +9,10 @@ def align_images(
 ) -> RgbImageCollection:
     """Align images to a reference image, and return the aligned image collection"""
     a_images = []
-    for img in tqdm(images):
-        reg_img, _ = aa.register(img, images[ref_image_index])
-        a_images.append(reg_img)
-
+    for i, img in tqdm(zip(range(len(images)), images), total=len(images)):
+        try: 
+            reg_img, _ = aa.register(img, images[ref_image_index])
+            a_images.append(reg_img)
+        except Exception as err:
+            print(f"Error {type(err)} in registration for image {i}")
     return RgbImageCollection(a_images)
