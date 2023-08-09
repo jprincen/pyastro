bb309094ca279d804c408bae869fcdf4fa59c8ac
John Princen <jprincen@gmail.com>
2023-08-08 17:30:25 -0700

    Separate scaling for rggb, maintain color balance

--- calibrate/calibrate_lights.py
+++ calibrate/calibrate_lights.py
@@ -19,11 +19,21 @@ def calibrate_lights(
     master_dark_uc = darks.mean()
     # Calibrated Master Flat
     master_flat_c = flats.mean() - master_bias
-    # Scaling value, selected to ensure mean value of calibrated light frame
-    # doesn't change after calibration
-    s0 = np.average(master_flat_c)
-    # Multiplicative factor
-    mxy = master_flat_c / s0
+    # Select different scaling value for each CFA element, so we don't alter
+    # the color balance of the result
+    f_red = master_flat_c[::2, ::2]
+    f_blue = master_flat_c[1::2, 1::2]
+    f_green_1 = master_flat_c[1::2, ::2]
+    f_green_2 = master_flat_c[::2, 1::2]
+    # Form master flat scaling
+    # TODO:::Should probably just use center of image, so
+    # heavy vignetting doesn't lead to under estimation of averages
+    mxy = np.zeros(master_flat_c.shape).astype(np.float32)
+    mxy[::2, ::2] = f_red / np.average(f_red)
+    mxy[1::2, 1::2] = f_blue / np.average(f_blue)
+    mxy[1::2, ::2] = f_green_1 / np.average(f_green_1)
+    mxy[::2, 1::2] = f_green_2 / np.average(f_green_2)
+
     # Now for each frame Ireal = (Idetected - master_dark_uc) / mxy
     # See reference
     lights_c = lights - master_dark_uc
